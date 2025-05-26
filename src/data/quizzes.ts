51b42fec35cdcdca64a6be75aaab5c0eb98f5513
egorarefev <e.arefjev@dvizh.io>
2025-05-26 13:29:28 +0300

    Создано приложение, добавлена структура

---
+++ src/data/quizzes.ts
@@ -0,0 +1,18 @@
+export const quizzes = [
+    {
+        id: 'html',
+        title: 'Основы HTML',
+        questions: [
+            {
+                text: 'Что такое тег <a>?',
+                options: ['Ссылка', 'Заголовок', 'Список'],
+                correctIndex: 0
+            },
+            {
+                text: 'Какой атрибут указывает путь на файл?',
+                options: ['src', 'href', 'alt'],
+                correctIndex: 1
+            },
+        ],
+    },
+];
