2db822edbf5a50172169f154307303f75e865c6a
egorarefev <e.arefjev@dvizh.io>
2025-06-11 14:02:58 +0300

    Добавлена форма логина самая простая

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainTemplate from './components/MainTemplate';
-import Home from './pages/Home';
 import Result from './pages/Result';
 import DictionaryPage from './pages/dictionary/DictionaryPage';
+import Login from "./pages/login/login";
 
 
 export default function App() {
@@ -10,7 +10,7 @@ export default function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<MainTemplate />}>
-                    <Route index element={<Home />} />
+                    <Route index element={<Login />} />
                     <Route path="result" element={<Result />} />
                     <Route path="/dictionary" element={<DictionaryPage />} />
                 </Route>
---
+++ src/pages/login/login.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import Button from '/src/components/ui/button/Button';
+import Input from '/src/components/ui/input/Input'
+
+export default function Login() {
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [isSignUp, setIsSignUp] = useState(false);
+
+    function onChangeLogin(e) {
+        setLogin(e.target.value);
+    }
+
+    function onChangePassword(e) {
+        setPassword(e.target.value);
+    }
+
+    function onChangeTypeLogin() {
+        setLogin('');
+        setPassword('');
+
+        setIsSignUp((prev) => !prev);
+    }
+
+    function onCreateAccount() {
+        console.log(`Аккаунт: ${login} создан`);
+    }
+
+    function onLogin() {
+        console.log(`Попытка входа в аккаунт: ${login}`);
+    }
+
+    return (
+        <div>
+            <div>Добро пожаловать в супер удобный инструмент</div>
+            <div>
+                {isSignUp ?
+                    (
+                        <div>
+                            <div>Регистрация</div>
+
+                            <p>
+                                Если у вас нет аккаунта, то создайте его,
+                                но запишите пароль потому что функцию восстановления мне делать лень пока что
+                            </p>
+                        </div>
+
+                    ) : (
+                        <div>
+                            <div>Вход</div>
+                            <p>
+                                Для входа в акк, введите логин и пароль
+                            </p>
+                        </div>
+
+                    )
+                }
+            </div>
+            <div>
+                <Input onChangeAction={onChangeLogin}/>
+                <Input type='password'/>
+            </div>
+            <div>
+                { isSignUp ?
+                    (<Button onClick={onChangeTypeLogin}>Есть аккаунт</Button>)
+                    : (<Button onClick={onLogin}>Войти</Button>)
+                }
+
+                { isSignUp ?
+                    (<Button onClick={onCreateAccount}>Создать аккаунт</Button>)
+                    : (<Button onClick={onChangeTypeLogin}>Зарегистрироваться</Button>)
+                }
+            </div>
+        </div>
+    )
+}
