3e008a4bb8ea089595bd7243cfb152db3fea6198
Egor Arefjev <egorarefjev@gmail.com>
2025-06-21 18:25:16 +0300

    Поработал со стилями

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -2,8 +2,8 @@ import styles from './button.module.scss';
 
 export default function Button({ children, onClick }) {
     return (
-            <div className={styles.button} onClick={onClick}>
+            <button className={styles.button} onClick={onClick}>
                 { children }
-            </div>
+            </button>
         );
 }
